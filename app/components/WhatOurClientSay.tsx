fa2da62abda93bf643ca9e14bf1307d9d232f17a
Khitish Kumar Pradhan <khitishkumarpradhan55@gmail.com>
2025-03-07 23:08:17 +0530

    minor changes done

--- app/components/WhatOurClientSay.tsx
+++ app/components/WhatOurClientSay.tsx
@@ -68,9 +68,7 @@ const WhatOurClientsSay: React.FC = () => {
       <div className="max-w-6xl mx-auto">
         <div className="text-center mb-12">
           <h2 className="text-4xl font-extrabold text-black mb-4 flex items-center justify-center">
-            <svg className="w-8 h-8 mr-3 text-indigo-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-              <path fillRule="evenodd" d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z" clipRule="evenodd" />
-            </svg>
+           
             What Our Clients Say
           </h2>
           <div className="w-24 h-1 bg-indigo-400 mx-auto rounded-full mb-6"></div>
