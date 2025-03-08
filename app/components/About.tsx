e69641745bd8099f7294673592e856343b9551fc
Khitish Kumar Pradhan <khitishkumarpradhan55@gmail.com>
2025-03-08 14:34:44 +0530

    button ui fixed

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -10,10 +10,8 @@ const About = () => {
       </div>
       <div className="sm:w-1/2 p-5">
         <div className="text">
-          <span className="text-gray-500 border-b-2 border-[#FFBF23] uppercase">
-            About us
-          </span>
-          <h2 className="my-4 font-bold text-3xl  sm:text-4xl ">
+          
+          <h2 className="my-4 font-bold text-3xl  sm:text-5xl ">
             About <span className="text-[#FFBF23]">Our Company</span>
           </h2>
           <p className="text-gray-700">
